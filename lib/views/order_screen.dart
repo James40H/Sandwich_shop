fbffcf28fd1081f397c8aafb7aaa1e8321810465
James  Hind <jameshind2005@gmail.com>
2025-11-28 14:35:44 +0000

    add a login button

--- lib/views/order_screen.dart
+++ lib/views/order_screen.dart
@@ -226,6 +226,13 @@ class _OrderScreenState extends State<OrderScreen> {
                 backgroundColor: Colors.blue,
               ),
               const SizedBox(height: 20),
+              StyledButton(
+                onPressed: _navigateToCartView,
+                icon: Icons.person,
+                label: 'Log In',
+                backgroundColor: Colors.purple,
+              ),
+              const SizedBox(height: 20),
               Text(
                 'Cart: ${_cart.countOfItems} items - £${_cart.totalPrice.toStringAsFixed(2)}',
                 style: normalText,
